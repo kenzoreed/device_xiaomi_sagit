529fa667344c15d58c0258d10ef06e71ef8322c9
kenzoreed <thesupernikkce@yandex.ru>
2020-06-14 17:59:21 +0000

    Ship MiuiCamera

--- bliss_sagit.mk
+++ bliss_sagit.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/xiaomi/sagit/device.mk)
 # Inherit some common BlissRoms stuff.
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/apps/MiuiCamera/config.mk)
+
 PRODUCT_NAME := bliss_sagit
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
